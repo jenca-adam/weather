831ea908182cbe60af6a133a01086e5cf4e14b05
Adam Jenča <jenca.adam@gmail.com>
2021-05-12 08:37:03 +0200

    avg fcst, docstring

--- src/weather.py
+++ src/weather.py
@@ -1,5 +1,6 @@
 import json
 
+import urllib3
 from bs4 import BeautifulSoup as bs
 from httplib2 import Http
 import httplib2
@@ -14,47 +15,85 @@ import re
 import platform
 import subprocess
 from pathlib import Path
+import selenium
+'''Python library for getting weather from different sources.
+Example:
+>>> import weather
+#Get temperature in Chicago tommorow at midnight ( Returns temperature in Fahrenheits )
+>>> weather.forecast("Chicago").tomorrow["0:00"].temp
+#Get temperature in current location today at noon
+>>> weather.forecast().today["12:00"].temp
+#Get precipitation probability after two days in Seoul at 4 o'clock
+>>> weather.forecast('Seoul').day(2)["16:00"].precip
+
+'''
 locator=Nominatim(user_agent="geoapiExercises")
 _h=Http()
 CELSIUS=0
 UNIT=CELSIUS
 FAHRENHEIT=1
 _DONTCHECK=-1
-
+'''Google chrome headers, used in BetaChrome'''
 _HDR={"User-Agent":"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.128 Safari/537.36"}
 class Browser:
+    '''Metaclass of BetaChrome used to bypass Google bot detection'''
     def request(self,url):
+        '''Request url and set USER-AGENT headers'''
         return _h.request(url,headers=self.HEADERS)
 class BetaChrome(Browser):
+    '''Used to bypass Google bot detection'''
     HEADERS=_HDR
-class DriverWarning(Warning):pass
-class WeatherWarning(Warning):pass
-class SVGWarning(WeatherWarning):pass
-class NoHeadlessWarning(DriverWarning):pass
-class WeatherError(BaseException):pass
-class NoSuchCityError(WeatherError):pass
-class DriverError(Exception):pass
+class DriverWarning(Warning):
+    '''Metaclass of NoHeadlessWarning'''
+class WeatherWarning(Warning):
+    '''Metaclass of SVGWarning'''
+class SVGWarning(WeatherWarning):
+    '''User is warned when SVG passed to analyzesvg() is already analyzed.
+    For debugging'''
+class NoHeadlessWarning(DriverWarning):
+    '''User is warned when no headless driver is not avaliable'''
+class WeatherError(BaseException):
+    '''Metaclass of WeatherError and  NoSuchCityError'''
+class NoSuchCityError(WeatherError):
+    '''Raised when city was not found using Nominatim'''
+class DriverError(Exception):
+    '''Raised when no driver is avaliable'''
 h=httplib2.Http()
-class IpError(ValueError):pass
-class Fail(IpError):pass
-class InvalidQuery(Fail):pass
-class ReservedRange(Fail):pass
-class SetupError(WeatherError):pass
-class ReadonlyError(Exception):pass
+class IpError(ValueError):
+    '''Metaclass of  Fail'''
+class Fail(IpError):
+    '''Metaclass of InvalidQuery and ReservedRange'''
+class InvalidQuery(Fail):
+    '''Raised when user entered non-existing IPv4 to track()'''
+class ReservedRange(Fail):
+    '''Raised when user entered 
+       localhost or 127.0.0.1 or 0.0.0.0 to track()'''
+
+class SetupError(WeatherError):
+    '''
+    Raised when setup.setup() was called but Chrome neither Firefox are installed
+    '''
+class ReadonlyError(Exception):
+    '''
+    Raised when user want to change Forecast, Day or SearchResult's attributes'''
 def searchurl(query):
+    '''Returns Google Search URL according to query'''  
     return f'https://google.com/search?q={query}'
-def getstate(city):
+def getcountry(city):
+    '''Return country that city is located in''' 
     return locator.geocode(city).address.split(',')[-1][1:]
 def _rq(h,u):
+    '''Handles IpApi ConnectionResetError'''
     try:
         return h.request(u)
     except ConnectionResetError:
         return _rq(h,u)
 def _track(ip):
+    '''Requests IpApi and returns result as dict'''
     r,c=_rq(h,f'http://ip-api.com/json/{ip}')
     return json.loads(c)
 def _checkresp(msg,stat):
-    
+    '''Raises error according to message and status returned by track()'''
     if stat=='success':return
     if stat=='fail':
         if msg=='reserved range':
@@ -64,18 +103,17 @@ def _checkresp(msg,stat):
         raise Fail(msg)
     raise IpError(msg)
 def checkresp(resp,ip=''):
-    if ip:
-        r2=track()
-        if r2==resp:
-            if r2.query!=resp.query:
-                raise InvalidQuery('invalid query')
+    '''Calls _checkresp with resp'''
+   
     if 'message' not in resp:return
     _checkresp(resp['message'],resp['status'])
 def track(ip=''):
+    '''Tracks according to IP. Used to detect location (when is called with no arguments,  returns data for current IP)'''
     resp=_track(ip)
     checkresp(resp,ip)
     return resp
 class Location:
+    '''Implementation of location'''
     def __init__(self,lat,lon,country,country_code,region_code,region,city,zipcode,tzone):
         self.lat=lat
         self.lon=lon
@@ -87,6 +125,7 @@ class Location:
         self.zipcode=zipcode
         self.tzone=tzone
 def _parse_loc(resp):
+    '''Returns Location object from data returned by track''' 
     return Location(
                     resp['lat'],
                     resp['lon'],
@@ -99,19 +138,26 @@ def _parse_loc(resp):
                     resp['timezone'],
                     )
 def iploc(ip):
+    '''Gets location according to IP'''
     return _parse_loc(track(ip))
 def curloc():
+    '''Gets current location'''
     return iploc('')
 _br=BetaChrome()
 request=_br.request
 def search(query):
+    '''Return search results from query'''
     return request(searchurl(query))[1]
 
-CITY=curloc().city
-COUNTRY=curloc().country
+### Set global variables according to current location
+LOCATION=curloc()
+CITY=LOCATION.city
+COUNTRY=LOCATION.country
+### Set temperature unit to Fahrenheit if user is in US
 if COUNTRY=='United States':
     UNIT=FAHRENHEIT
 def refresh(ip=''):
+    '''Gets city and country from current location(or IP location'''
     if not ip:
         city=curloc().city
         country=curloc().country
@@ -121,14 +167,18 @@ def refresh(ip=''):
 
     return city,country
 class _WeatherChannel:
+    '''weather.google type'''
     def __init__(self):
+        '''Set driver to None for fututre forecast requests will be faster'''
         self.driver=None
     class Weather:
+        '''Implementation of weather'''
         def __init__(self,wind,precip,temp):
             self.temp=temp
             self.precip=precip
             self.wind=wind
     class Day:
+        '''Implementation of day'''
         def __init__(self,weatherlist,wdict):
             '''
             self.highest=max(
@@ -140,8 +190,10 @@ class _WeatherChannel:
             self.weather_as_list=weatherlist
             self.weather_as_dict=wdict
         def __getitem__(self,i):
+            '''Return weather at time'''
             return self.gettemp(i)
         def splittime(self,time,after=int):
+            '''Splits time to hours and minutes and calling to result after'''
             try:
                 return tuple(after(i) for i in time.split(':'))
             except ValueError:
@@ -149,27 +201,34 @@ class _WeatherChannel:
                                 f"invalid value for 'splittime':{time}"
                                 )
         def fillin(self,time):
+            '''Fills the time in. E.g.:fillin("8:00")="08:00"'''
             return self.jointime(self.formatsec(i) for i in  self.splittime(time,after=str))
         def formatsec(self,time):
+            '''Formats seconds E.g:formatsec("8")="08"'''
             if len(time)>2:
                 raise ValueError('not 2-digit or 1-digit time')
             if len(time)<2:
                 return "0"+time
             return time
         def jointime(self,time):
+            '''Gets analog time according to hours and minutes'''
             return ':'.join(str(i) for i in time)
         def gettemp(self,time):
+            '''Returns weather at time'''
             if isinstance(time,str):
                 time=self.splittime(time)
             return self.weather_as_dict[self.fillin(self.repairtime(self.jointime(time)))]
         def timetoint(self,time):
+            '''Converts time to integer'''
             if isinstance(time,str):
                 return self.splittime(time)[1]+self.splittime(time)[0]*60
             return time[1]+time[0]*60
         def inttotime(self,i):
+            '''Converts integer to time. Reverse function to timetoint'''
             return (i//60,i%60)
 
         def repairtime(self,time):
+            '''Gets closest time that is in weather list'''
             closest=lambda num,collection:min(collection,key=lambda x:abs((x-num)+1))
             dy=self.weather_as_dict
 
@@ -178,16 +237,17 @@ class _WeatherChannel:
             return self.jointime(self.inttotime(closest(qr,dy)))
             
         def roundtime(self,time):
-
+            '''Rounds time; e.g. roundtime("8:10")="08:00"'''
             mins=int(time.split(':')[-1])
             if mins==0:
                 return time
             hrs=int(time.split(':')[0])
-            if mins<50:
+            if mins<30:
                 return f'{self.formatsec(hrs)}:00'
             return f'{self.formatsec(hrs+1)}:00'
 
     def convert(self,d):
+        ''' Converts dictionary to JSON serializable form'''
         l={}
         for a in d:
             i=d[a]
@@ -199,6 +259,7 @@ class _WeatherChannel:
             l[a]=m
         return l    
     def convlist(self,d):
+        '''Converts list to JSON serializable form'''
         l=[]
         for i in d:
             m=i
@@ -207,6 +268,7 @@ class _WeatherChannel:
             l.append(m)
         return l
     class Forecast:
+        '''Implemantation of weather forecast'''
         def __init__(self,temp,today,nxt):
             e=None
             if not isinstance(today,_WeatherChannel.Day):
@@ -227,6 +289,7 @@ class _WeatherChannel:
             self.days=nxt
             self.tomorrow=nxt[0]
         def day(self,num):
+            '''Returns weather at day n'''
             return self.days[num]
         def splittime(self,time,after=int):
             try:
@@ -281,8 +344,10 @@ class _WeatherChannel:
                                 )
 
     def parsetemp(self,t,unit=UNIT):
+        '''Parses temperature HTML'''
         return int(t.find_all(class_="wob_t")[unit].text)
     def parseday(self,d,unit=UNIT):
+        '''Parses one day'''
         s=bs(str(d),'html.parser')
         dayname=s.find(class_="QrNVmd Z1VzSb")['aria-label']
         desc=s.find(class_="DxhUm").img['alt']
@@ -291,6 +356,7 @@ class _WeatherChannel:
         lowest=self.parsetemp(tmps.find(class_="QrNVmd ZXCv8e"),unit=unit)
         return self.Day(dayname,highest,lowest,desc)
     def getsvg(self,ch,unit):
+        '''Gets SVG with temperature'''
         try:
                 ch.find_elements_by_class_name("jyfHyd")[1].click()
         except:pass
@@ -300,6 +366,7 @@ class _WeatherChannel:
         svg=svg.get_attribute('outerHTML')
         return self.analyzesvg(svg,unit)
     def getprecip(self,ch):
+        '''Gets precipitation data'''
         precip_html_element=ch.find_element_by_id('wob_pg')
         precip_html=precip_html_element.get_attribute('outerHTML')
         precip_soup=bs(precip_html,
@@ -324,6 +391,7 @@ class _WeatherChannel:
 
             
     def getwind(self,ch):
+        '''Gets wind data''' 
         wind_html_element=ch.find_element_by_id('wob_wg')
         wind_html=wind_html_element.get_attribute('outerHTML')
         wind_soup=bs(wind_html,
@@ -343,6 +411,7 @@ class _WeatherChannel:
 
         return days
     def getgraph(self,ch,unit):
+        '''Gets full data and formats them into Forecast object'''
         svg=self.getsvg(ch,unit)
         precip=self.getprecip(ch)
         wind=self.getwind(ch)
@@ -402,6 +471,7 @@ class _WeatherChannel:
 
                                      
     def analyzesvg(self,svg,unit):
+        '''Changes svg to dict of temperatures'''
         if isinstance(svg,list) :
             warnings.warn(
                           SVGWarning(
@@ -431,10 +501,12 @@ class _WeatherChannel:
         return days
 
     def parsefcast(self,days,temp,unit=CELSIUS):
+        '''Parses forecast'''
         first=days[0]
         nxt=days[1:]
         return self.Forecast(temp,first,nxt)
     def forecast(self,cityname=CITY,countryname='',unit=None):
+        '''Gets forecast'''
         err=None
         if self.driver is None:
             driver=_driverSearch()
@@ -442,7 +514,7 @@ class _WeatherChannel:
         wd=self.driver
         if not countryname:
             try:
-                countryname=getstate(cityname)
+                countryname=getcountry(cityname)
             except AttributeError:
                 err=NoSuchCityError(f"no such city: '{cityname}'")
         if err:
@@ -484,7 +556,12 @@ class _WeatherChannel:
                             'Could not parse temperature string')
         return int(match.group(0).replace('°',''))
 class _YR_NORI:
+    '''yr.no source'''
+    def __init__(self):
+        self.driver=None
+        self.ForecastParser=self._ForecastParser()
     class SearchResults:
+        '''Implementation of search results'''
         def __init__(self,l):
             self.res=l
             self.first=l[0]
@@ -497,6 +574,7 @@ class _YR_NORI:
         def __repr__(self):
             return repr(self.res)
     class Day:
+        '''Implementation of one day'''
         def __init__(self,wlst,wdict):
             self.wlst=wlst
             self.wdict=wdict
@@ -550,6 +628,7 @@ class _YR_NORI:
             return f'{self.formatsec(hrs+1)}:00'
 
     class Forecast:
+        '''implementation of weather forecast'''
         def __init__(self,days):
             self.days=days
         def __getitem__(self,i):
@@ -559,11 +638,40 @@ class _YR_NORI:
 
         def day(self,daynum):
             return self.days[daynum]
+    
+    class _ForecastParser:
+        '''Parses forecast'''
+        def parse(self,driver,u):   
+            '''Gets temperature'''
+            temps_final=[]
+            time_final=[]
+            for i in range(9):
+                driver.get(f'{u}?i={i}')
+                time.sleep(0.1)
+                hlava=driver.page_source
+                     
+                sp=bs(hlava,
+                      'html.parser'
+                      )
+                temps=sp.find_all("span", class_="temperature temperature--warm")
+                temps_final.append([parser.detectint(t.text) for t in temps])
+            return temps_final
+        def close_ad(self,driver):
+            '''Closes "We have a new graph" pop-up'''
+            try:
+                driver.find_elements_by_class_name("feature-promo-modal-meteogram__link")[0].click()
+            except:pass
+
+
+                
     def searchurl(self,q):
-        return f'https://yr.no/en/search?q={q}'
+        '''Returns yr.no search URL'''
+        return f'https://www.yr.no/en/search?q={q}'
     def expandhref(self,href):
-        return f'https://yr.no{href}'
+        '''Expands href'''
+        return f'https://www.yr.no{href}'
     def search(self,q):
+        '''Searches yr.no'''
         self.driver=_driverSearch().best()
         self.driver.get(
             self.searchurl(q)
@@ -573,15 +681,15 @@ class _YR_NORI:
         results=results.findAll('li')
         results=[self.expandhref(result.a['href']) for result in results]
         return self.SearchResults(results) 
-    def forecast(self,cityname=CITY,countryname=COUNTRY):
+    def forecast(self,cityname=CITY,countryname=COUNTRY,unit=None):
+        '''Gets forecast'''
         err=None
         if self.driver is None:
             driver=_driverSearch()
             self.driver=driver.best()
-        wd=self.driver
         if not countryname:
             try:
-                countryname=getstate(cityname)
+                countryname=getcountry(cityname)
             except AttributeError:
                 err=NoSuchCityError(f"no such city: '{cityname}'")
         if err:
@@ -596,11 +704,16 @@ class _YR_NORI:
         if countryname==_DONTCHECK:
             query=f'weather {cityname}'
         else:
-            query=f'weather {cityname} {countryname}'
-        c=search(query).result(0)
-        return self.ForecastParser(c)
-
+            query=f'{cityname} {countryname}'
+        c=self.search(query).first.split('/')
+        c[5]="hourly-table"
+        print('/'.join(c))
+        try:
+            self.driver.find_elements_by_class_name("feature-promo-modal-meteogram__link")[0].click()
+        except:pass
+        return self.ForecastParser.parse(self.driver,'/'.join(c))
 class _driverSearch:
+    '''Search drivers'''
     def __init__(self):
         
         self.browsers=[Chrome,Firefox,Safari,Ie,Edge,PhantomJS]
@@ -700,6 +813,7 @@ class _driverSearch:
             if i[0]==dr:
                 return i[1]
     def best(self):
+        '''Get best driver'''
         if '.weather' not in os.listdir():
             os.mkdir('.weather')
             hdlsxst=False
@@ -750,6 +864,7 @@ class _driverSearch:
                     return b(options=self._gethopt(b))
             return b()
 class _SetterUp:
+    '''Sets up chromedriver and geckodriver'''
     SYSTEM=platform.system()
     if SYSTEM=='Windows':
         CHROMEDRIVER_URL="https://github.com/jenca-adam/jenca-adam.github.io/raw/master/projects/weather/extras/bin/win/chromedriver.exe"
@@ -838,6 +953,7 @@ class _SetterUp:
                 
         
 class _cukor:
+    '''Advanced list comprehension'''
     def cukor(a,b,c,d,e):
         result=[]
         exec(
@@ -854,8 +970,69 @@ class _cukor:
                 
 
 
-        
+class parser:
+    '''Parses temperature and time strings'''
+    class ParsingError(ValueError):pass
+    def detectint(self,st):
+        p=re.compile(r"(\-?\d+)")
+        match=p.search(st)
+        if not match:
+            raise self.ParsingError(
+                                    "no int detected"
+                                    )
+        return int(match.group(0))
+    def is_inaccurate(self,st):
+        i=self.detectint(st)
+        return '-' in st
+    def parse_inaccurate(self,t):
+        if not self.is_inaccurate(t):
+            try:
+                return int(t)
+            except ValueError:
+                raise self.ParsingError("not a valid time")
 
+        else:
+            f,l=t.split('-')
+            try:
+                f=int(f)
+                l=int(l)
+            except ValueError:
+                raise self.ParsingError("not a valid time")
+            d=(l-f)//2
+            if d<0:
+                raise self.ParsingError(
+                "not a valid time -- second int is larger than first")
+            return f+d
+class _Avg:
+    '''get best forecast result'''
+    SVC=['google']
+    def forecast(self,*a,**k):
+        for service in self.SVC:
+            try:
+                return SERVICES[service].forecast(*a,**k)
+            except KeyboardInterrupt:
+                raise SetupError("lost connection to service")
+            except urllib3.exceptions.MaxRetryError:
+                raise DriverError(
+                                  """Because of unknown error in Selenium was lost connection to driver.
+                                     Consider restarting your script/module"""
+                                     )
+            except WeatherError:
+                pass
+            except selenium.common.exceptions.WebDriverException:
+                raise DriverError(
+                                 """Could not set up the driver, probably keyboard interrupt?"""
+                                 )
+            except Exception:
+                if e.__class__==KeyboardInterrupt:
+                    raise KeyboardInterrupt("interrupted while searching for forecast")
+        raise WeatherError("could not find service matching your search")
+parser=parser()
 google=_WeatherChannel()
 yrno=_YR_NORI()
 setup=_SetterUp()
+average=_Avg()
+SERVICES={'google':google}
+def forecast(service=average):
+    return service.forecast()
+
